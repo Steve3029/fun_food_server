b83c28deef8984b7355060da75d67c898521c3ed
Steve <steve.liu.3029@gmail.com>
2018-09-14 13:39:35 +1200

    separating recipe maker into a independent object

---
+++ FunFoodServer.Domain/Model/Maker.cs
@@ -0,0 +1,11 @@
+﻿
+namespace FunFoodServer.Domain.Model
+{
+    public class Maker : AggregateRoot
+    {
+      public string UserName { get; set; }
+
+      public string PhotoUrl { get; set; }
+  
+    }
+}
--- FunFoodServer.Domain/Model/Recipe.cs
+++ FunFoodServer.Domain/Model/Recipe.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
 
 namespace FunFoodServer.Domain.Model
 {
@@ -22,6 +20,6 @@ namespace FunFoodServer.Domain.Model
 
       public List<Instruction> Instructions { get; set; }
 
-      public User Author { get; set; }
+      public Maker Maker { get; set; }
     }
 }
