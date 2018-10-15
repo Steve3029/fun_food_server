82f6341e0e15e20e8b725a3c63f0e7bb77881137
steve3029 <steve.liu.3029@gmail.com>
2018-10-15 14:23:16 +1300

    add unit tests for classes in specifications package

---
+++ FunFoodServer.Domain.Tests/Specifications/Tests.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using FunFoodServer.Domain.Model;
+using Xunit;
+
+namespace FunFoodServer.Domain.Tests.Specifications
+{
+  public class Tests
+  {
+    [Fact]
+    public void CompositeTest()
+    {
+      var category = new Category() { Id = Guid.NewGuid(), Name = "side" };
+      var recipe = new Recipe() { Id = Guid.NewGuid(), Title = "fried eggs", Category = category };
+      var titleSpecification = new TitleSpecification("egg");
+      var categorySpecification = new CategorySpecification("side");
+
+      var composed = titleSpecification.And(categorySpecification);
+      bool isSatisfiedBy = composed.IsSatisfiedBy(recipe);
+
+      Assert.True(isSatisfiedBy);
+    }
+  }
+}
