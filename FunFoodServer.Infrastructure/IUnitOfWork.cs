f9bb953ca8530b4a89df608e2101ac4d5f54d327
steve3029 <steve.liu.3029@gmail.com>
2018-11-22 14:29:39 +1300

    add unit of work interface to prepare for unit of work pattern

---
+++ FunFoodServer.Infrastructure/IUnitOfWork.cs
@@ -0,0 +1,19 @@
+﻿using FunFoodServer.Domain;
+
+namespace FunFoodServer.Infrastructure
+{
+  public interface IUnitOfWork
+  {
+
+    void RegisterNew<TAggregateRoot>(TAggregateRoot entity)
+      where TAggregateRoot : class, IAggregateRoot;
+
+    void RegisterModified<TAggregateRoot>(TAggregateRoot entity)
+      where TAggregateRoot : class, IAggregateRoot;
+
+
+    void RegisterDelete<TAggregateRoot>(TAggregateRoot entity)
+      where TAggregateRoot : class, IAggregateRoot;
+
+  }
+}
