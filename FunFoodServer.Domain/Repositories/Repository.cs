5adca9680cc081059900da5d3f1dc105f95f84b1
steve3029 <steve.liu.3029@gmail.com>
2018-12-04 14:22:29 +1300

    adds repository abstract class as a base class for specific implementation

---
+++ FunFoodServer.Domain/Repositories/Repository.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using FunFoodServer.Domain.Specifications;
+
+namespace FunFoodServer.Domain.Repositories
+{
+  public abstract class Repository<TAggregateRoot> : IRepository<TAggregateRoot>
+  {
+    private readonly IRepositoryContext _context;
+
+    public Repository(IRepositoryContext context)
+    {
+      this._context = context;
+    }
+    // Adds an aggregate root to repo
+    protected abstract void DoAdd(TAggregateRoot aggregateRoot);
+    // Gets an aggregate root from repo by a given key
+    protected abstract TAggregateRoot DoGetByKey(Guid Id);
+    // Gets all aggregate roots from repo
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll()
+    {
+      return DoGetAll(new AnySpecification<TAggregateRoot>(), null, SortOrder.Unspecified);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(int pageNumber, int pageSize)
+    {
+      return DoGetAll(new AnySpecification<TAggregateRoot>(), null, SortOrder.Unspecified, pageNumber, pageSize);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(Expression<Func<TAggregateRoot, dynamic>> sortPredicate, SortOrder sortOrder)
+    {
+      return DoGetAll(new AnySpecification<TAggregateRoot>(), sortPredicate, sortOrder);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(Expression<Func<TAggregateRoot, dynamic>> sortPredicate, SortOrder sortOrder, int pageNumnber, int pageSize)
+    {
+      return DoGetAll(new AnySpecification<TAggregateRoot>(), sortPredicate, sortOrder, pageNumnber, pageSize);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(ISpecification<TAggregateRoot> specification)
+    {
+      return DoGetAll(specification, null, SortOrder.Unspecified);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(ISpecification<TAggregateRoot> specification, int pageNumber, int pageSize)
+    {
+      return DoGetAll(specification, null, SortOrder.Unspecified, pageNumber, pageSize);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(params Expression<Func<TAggregateRoot, dynamic>>[] eagerLoadingProperties)
+    {
+      return DoGetAll(new AnySpecification<TAggregateRoot>(), null, SortOrder.Unspecified, eagerLoadingProperties);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(int pageNumber, int pageSize, params Expression<Func<TAggregateRoot, dynamic>>[] eagerLoadingProperties)
+    {
+      return DoGetAll(new AnySpecification<TAggregateRoot>(), null, SortOrder.Unspecified, pageNumber, pageSize, eagerLoadingProperties);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(Expression<Func<TAggregateRoot, dynamic>> sortPredicate, SortOrder sortOrder, params Expression<Func<TAggregateRoot, dynamic>>[] eagerLoadingProperties)
+    {
+      return DoGetAll(new AnySpecification<TAggregateRoot>(), sortPredicate, sortOrder, eagerLoadingProperties);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(Expression<Func<TAggregateRoot, dynamic>> sortPredicate, SortOrder sortOrder, int pageNumber, int pageSize, params Expression<Func<TAggregateRoot, dynamic>>[] eagerLoadingProperties)
+    {
+      return DoGetAll(new AnySpecification<TAggregateRoot>(), sortPredicate, sortOrder, eagerLoadingProperties);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(ISpecification<TAggregateRoot> specification, params Expression<Func<TAggregateRoot, dynamic>>[] eagerLoadingProperties)
+    {
+      return DoGetAll(specification, null, SortOrder.Unspecified, eagerLoadingProperties);
+    }
+
+    protected virtual IEnumerable<TAggregateRoot> DoGetAll(ISpecification<TAggregateRoot> specification, int pageNumber, int pageSize, params Expression<Func<TAggregateRoot, dynamic>>[] eagerLoadingProperties)
+    {
+      return DoGetAll(specification, null, SortOrder.Unspecified, pageNumber, pageSize, eagerLoadingProperties);
+    }
+
+    protected abstract IEnumerable<TAggregateRoot> DoGetAll(ISpecification<TAggregateRoot> specification, Expression<Func<TAggregateRoot, dynamic>> sortPredicate, SortOrder sortOrder);
+
+    protected abstract IEnumerable<TAggregateRoot> DoGetAll(ISpecification<TAggregateRoot> specification, Expression<Func<TAggregateRoot, dynamic>> sortPredicate, SortOrder sortOrder, int pageNumber, int pageSize);
+
+    protected abstract IEnumerable<TAggregateRoot> DoGetAll(ISpecification<TAggregateRoot> specification, Expression<Func<TAggregateRoot, dynamic>> sortPredicate, SortOrder sortOrder, params Expression<Func<TAggregateRoot, dynamic>>[] eagerLoadingProperties);
+
+    protected abstract IEnumerable<TAggregateRoot> DoGetAll(ISpecification<TAggregateRoot> specification, Expression<Func<TAggregateRoot, dynamic>> sortPredicate, SortOrder sortOrder, int pageNumber, int pageSize, params Expression<Func<TAggregateRoot, dynamic>>[] eagerLoadingProperties);
+
+  }
+}
---
+++ FunFoodServer.Domain/Specifications/AnySpecification.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace FunFoodServer.Domain.Specifications
+{
+  public class AnySpecification<T> : Specification<T>
+  {
+    public override Expression<Func<T, bool>> ToExpression()
+    {
+      return p => true;
+    }
+  }
+}
