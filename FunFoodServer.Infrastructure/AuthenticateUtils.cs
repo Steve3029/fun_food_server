30cbcfb4816a0b014cc3cc67a119dbf6371a05c7
steve3029 <steve.liu.3029@gmail.com>
2018-12-18 18:05:01 +1300

    add JWT authentication feature

---
+++ FunFoodServer.Infrastructure/AuthenticateUtils.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+using FunFoodServer.Domain.Model;
+using Microsoft.IdentityModel.Tokens;
+
+namespace FunFoodServer.Infrastructure
+{
+  public static class AuthenticateUtils
+  {
+
+    public static string GenerateTokenForUser(User user, string Secret)
+    {
+      // generating token for user based on related data of user, like name, id
+      var tokenHandler = new JwtSecurityTokenHandler();
+      var key = Encoding.UTF8.GetBytes(Secret);
+      var tokenDescriptor = new SecurityTokenDescriptor()
+      {
+        Subject = new ClaimsIdentity(new Claim[]
+        {
+          new Claim(ClaimTypes.Name, user.Id.ToString())
+        }),
+        Expires = DateTime.UtcNow.AddDays(3),
+        SigningCredentials = new SigningCredentials(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+      };
+      var token = tokenHandler.CreateToken(tokenDescriptor);
+      return tokenHandler.WriteToken(token);
+    }
+  }
+}
